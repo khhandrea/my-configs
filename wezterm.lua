26f1ddf2c24d6332539cc52ff8e832b258cf0242
khhandrea <khhandrea@kakao.com>
2025-12-06 20:51:23 +0900

    Modify indentation

--- wezterm.lua
+++ wezterm.lua
@@ -5,14 +5,14 @@ local config = wezterm.config_builder()
 
 -- Set default shell
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    -- Windows
-    config.default_prog = {"powershell.exe"}
+  -- Windows
+  config.default_prog = {"powershell.exe"}
 elseif wezterm.target_triple:find("darwin") then
-    -- MacOS
-    config.default_prog = {"/bin/zsh", "-l"}
+  -- MacOS
+  config.default_prog = {"/bin/zsh", "-l"}
 else
-    -- Linux
-    config.default_prog = {"/bin/bash", "-l"}
+  -- Linux
+  config.default_prog = {"/bin/bash", "-l"}
 end
 
 -- Font
@@ -25,8 +25,8 @@ config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 
 config.colors = {
-    foreground = "#D8D8D0", 
-    background = "#222420",
+  foreground = "#D8D8D0", 
+  background = "#222420",
 	cursor_bg = "#50D0A0",
 	cursor_fg = "#011423",
 	selection_bg = "#033259",
@@ -34,15 +34,15 @@ config.colors = {
 }
 
 config.inactive_pane_hsb = {
-        saturation = 0.8,
-        brightness = 0.9,
+  saturation = 0.8,
+  brightness = 0.9,
 }
 
 config.window_padding = {
-        left = 12,
-        right = 4,
-        top = 4,
-        bottom = 12,
+  left = 12,
+  right = 4,
+  top = 4,
+  bottom = 12,
 }
 
 -- opacity range [0, 1]
@@ -54,13 +54,13 @@ config.macos_window_background_blur = 10
 -- Keybindings
 local act = wezterm.action
 config.keys = {
-    { key = "h", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Left") },
-    { key = "l", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Right") },
-    { key = "k", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Up") },
-    { key = "j", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Down") },
-    { key = '"', mods = "CTRL|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-    { key = "%", mods = "CTRL|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-    { key = "d", mods = "CTRL|SHIFT", action = act.CloseCurrentPane({ confirm = true }) },
+  { key = "h", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Left") },
+  { key = "l", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Right") },
+  { key = "k", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Up") },
+  { key = "j", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Down") },
+  { key = '"', mods = "CTRL|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "%", mods = "CTRL|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "d", mods = "CTRL|SHIFT", action = act.CloseCurrentPane({ confirm = true }) },
 }
 
 return config
